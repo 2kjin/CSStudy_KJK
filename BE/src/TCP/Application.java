03226ab88aca7afce176bf5f3883bea56b39d10f
2kjin <lkjin1202@gmail.com>
2024-07-01 01:19:36 +0900

    ✨ feat : 사용자 입력을 받아 메뉴 선택 처리하는 메인 함수 설계

--- BE/src/TCP/Application.java
+++ BE/src/TCP/Application.java
@@ -1,4 +1,20 @@
 package TCP;
 
+import service.Controller;
+
+import java.util.Scanner;
+
 public class Application {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Controller controller = new Controller();
+
+        while (true){
+//            controller.showMenu();
+            int num = sc.nextInt();
+//            controller.choiceMenu(num);
+        }
+    }
 }
