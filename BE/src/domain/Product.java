25e0e88002e05f08a97c416f24dab4364ba9ca2f
2kjin <lkjin1202@gmail.com>
2024-07-01 01:27:10 +0900

    ✨ feat : 재고 변수명 변경 & 상품 리스트 출력 및 사용자 메뉴 인터페이스 구현

--- BE/src/domain/Product.java
+++ BE/src/domain/Product.java
@@ -5,37 +5,7 @@ public class Product {
     private int productNumber;
     private String name;
     private int price;
-    private int inventory;
+    private int stock;
 
-    public int getProductNumber() {
-        return productNumber;
-    }
 
-    public void setProductNumber(int productNumber) {
-        this.productNumber = productNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(int inventory) {
-        this.inventory = inventory;
-    }
 }
--- BE/src/service/Controller.java
+++ BE/src/service/Controller.java
@@ -1,4 +1,26 @@
 package service;
 
+import domain.Product;
+
+import java.util.Scanner;
+
 public class Controller {
+
+    Scanner sc = new Scanner(System.in);
+    Product p = new Product();
+
+    //    p.set(sc.nextLine());
+    String[][] ProductList = new String[100][3];
+    int productNumber = 0;
+
+    public void getProductList(){
+        System.out.println("---------------------------------------------------------");
+        System.out.println("no       name                     price         stock    ");
+        System.out.println("---------------------------------------------------------");
+
+        System.out.println("---------------------------------------------------------");
+        System.out.print("메뉴 : 1. Create | 2. Update | 3. Delete | 4. Exit");
+        System.out.print("선택 : ");
+    }
+
 }
