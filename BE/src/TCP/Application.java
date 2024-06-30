ac07010babed70612ef38229fecd5706d9d4569e
2kjin <lkjin1202@gmail.com>
2024-07-01 01:17:44 +0900

    ✨ feat : Product 클래스와 속성, 접근자 메소드 추가

--- BE/src/TCP/Application.java
+++ BE/src/TCP/Application.java
@@ -1,12 +1,4 @@
 package TCP;
 
-import java.util.Scanner;
-
 public class Application {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-
-    }
 }
--- BE/src/domain/Product.java
+++ BE/src/domain/Product.java
@@ -2,4 +2,40 @@ package domain;
 
 public class Product {
 
+    private int productNumber;
+    private String name;
+    private int price;
+    private int inventory;
+
+    public int getProductNumber() {
+        return productNumber;
+    }
+
+    public void setProductNumber(int productNumber) {
+        this.productNumber = productNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
 }
